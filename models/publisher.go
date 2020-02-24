ccc637d78a176c4a7b21047d982b718444fcaefe
Bernard Mulobi <bmulobi@gmail.com>
2020-02-24 23:41:11 +0300

    Cleaned up code
    Signed-off-by: Bernard Mulobi <bmulobi@gmail.com>

--- models/publisher.go
+++ models/publisher.go
@@ -2,6 +2,6 @@ package models
 
 // Publisher is the model for publishers
 type Publisher struct {
-	Id uint `gorm:"primary_key"json:"id,omitempty"`
+	Id   uint   `gorm:"primary_key"json:"id,omitempty"`
 	Name string `gorm:"type:varchar(100);not null;unique"json:"name,omitempty"`
 }
